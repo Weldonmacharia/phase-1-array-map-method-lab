21e49d7825348e4c22a5d6136f9464e4199ffc21
Weldon Macharia <weldonmacharia28@gmail.com>
2025-06-29 04:13:30 +0300

    workd

--- index.js
+++ index.js
@@ -11,6 +11,32 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+const specialCases = {
+  oo: "OO",
+  api: "API",
+  nan: "NaN",
+  stoppropagation: "StopPropagation",
+  preventdefault: "PreventDefault",
+  jsonp: "JSONP"
+};
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(title => {
+    return title
+      .split(' ')
+      .map(word => {
+        // strip punctuation from the end, keep it aside
+        const punctuation = word.match(/[^a-zA-Z]*$/)[0] || '';
+        const coreWord = word.slice(0, word.length - punctuation.length);
+
+        const lower = coreWord.toLowerCase();
+
+        if (specialCases.hasOwnProperty(lower)) {
+          return specialCases[lower] + punctuation;
+        } else {
+          return coreWord.charAt(0).toUpperCase() + coreWord.slice(1).toLowerCase() + punctuation;
+        }
+      })
+      .join(' ');
+  });
+};
